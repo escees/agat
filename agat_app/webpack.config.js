8ee7fe6eb4fbe0131a8d6cd86ef5f878b1d3edb6
kamil.scislowski <kamil.scislowski@xsolve.pl>
2018-07-17 09:00:13 +0200

    removed unused things, added webpack config comment

--- agat_app/webpack.config.js
+++ agat_app/webpack.config.js
@@ -14,15 +14,13 @@ Encore
     // uncomment to define the assets of the project
     .addEntry('js/bootstrap', './node_modules/bootstrap/dist/js/bootstrap.bundle.js')
     .addEntry('js/homepage', './assets/js/homepage.js')
-    .addEntry('js/mdb', './assets/js/mdb.js')
+    .addEntry('js/mdb', './assets/js/mdb.js') //this should also be loaded from vendors
     .addEntry('js/bundle', './assets/js/app.js')
-    // .addEntry('js/style-bundle', './assets/scss/app.scss')
-    .addEntry('js/popper', './assets/js/popper.min.js')
+    .addEntry('js/popper', './node_modules/popper.js/dist/popper.js')
     .addEntry('js/jquery', './node_modules/jquery/dist/jquery.js')
     .addStyleEntry('css/bootstrap', './node_modules/bootstrap/dist/css/bootstrap.css') // it is possible to include vendors directly from node_modules - no need to copy them into assets
     .addStyleEntry('css/mdb', './assets/css/mdb.css')
     .addStyleEntry('css/style', './assets/css/style.css')
-    // .addStyleEntry('css/bundle', './assets/css/app.scss')
     .addPlugin(new webpack.ProvidePlugin({
         $: 'jquery',
         jQuery: 'jquery',
