1faa370b1ec7e76c806177c95e83f27afa14a175
kamil.scislowski <kamil.scislowski@xsolve.pl>
2018-07-17 15:29:21 +0200

    webpack unused file removed from config

--- agat_app/webpack.config.js
+++ agat_app/webpack.config.js
@@ -15,7 +15,6 @@ Encore
     .addEntry('js/bootstrap', './node_modules/bootstrap/dist/js/bootstrap.bundle.js')
     .addEntry('js/homepage', './assets/js/homepage.js')
     .addEntry('js/mdb', './assets/js/mdb.js') //this should also be loaded from vendors
-    .addEntry('js/bundle', './assets/js/app.js')
     .addEntry('js/popper', './node_modules/popper.js/dist/popper.js')
     .addEntry('js/jquery', './node_modules/jquery/dist/jquery.js')
     .addStyleEntry('css/bootstrap', './node_modules/bootstrap/dist/css/bootstrap.css') // it is possible to include vendors directly from node_modules - no need to copy them into assets
