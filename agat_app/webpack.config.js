c5ef8ab73a901784576e4079a183de9a97de3a44
kamil.scislowski <kamil.scislowski@xsolve.pl>
2018-07-18 12:14:11 +0200

    added wow.js from cdn, adjust loading performance by using minimized files

--- agat_app/webpack.config.js
+++ agat_app/webpack.config.js
@@ -12,14 +12,13 @@ Encore
     // .enableVersioning(Encore.isProduction())
 
     // uncomment to define the assets of the project
-    .addEntry('js/bootstrap', './node_modules/bootstrap/dist/js/bootstrap.bundle.js')
+    .addEntry('js/bootstrap', './node_modules/bootstrap/dist/js/bootstrap.bundle.min.js')
     .addEntry('js/homepage', './assets/js/homepage.js')
-    .addEntry('js/mdb', './assets/js/mdb.js') //this should also be loaded from vendors
-    .addEntry('js/popper', './node_modules/popper.js/dist/popper.js')
-    .addEntry('js/jquery', './node_modules/jquery/dist/jquery.js')
-    .addStyleEntry('css/bootstrap', './node_modules/bootstrap/dist/css/bootstrap.css') // it is possible to include vendors directly from node_modules - no need to copy them into assets
-    .addStyleEntry('css/mdb', './assets/css/mdb.css')
-    .addStyleEntry('css/style', './assets/css/style.css')
+    .addEntry('js/mdb', './assets/js/mdb.min.js') //this should also be loaded from vendors
+    .addEntry('js/jquery', './node_modules/jquery/dist/jquery.min.js')
+    .addStyleEntry('css/bootstrap', './node_modules/bootstrap/dist/css/bootstrap.min.css') // it is possible to include vendors directly from node_modules - no need to copy them into assets
+    .addStyleEntry('css/mdb', './assets/css/mdb.min.css')
+    .addStyleEntry('css/style', './assets/css/style.min.css')
     .addPlugin(new webpack.ProvidePlugin({
         $: 'jquery',
         jQuery: 'jquery',
